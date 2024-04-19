36c41dfb5be788e0c09b0063c994a5cf23f2f56c
Tomdieu <ivantomdio@gmail.com>
2024-04-19 19:21:18 +0100

    feat : added logs & k8s

---
+++ backend/users/logs/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger("users")
+
+handler = logging.FileHandler('logs/users.log')
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
