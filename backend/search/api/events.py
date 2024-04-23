a4caef8338c4552a069a7dadbcc405a2db490d1d
Tomdieu <ivantomdio@gmail.com>
2024-04-23 07:03:24 +0100

    feat : added events type for search svc

---
+++ backend/search/api/events.py
@@ -0,0 +1,11 @@
+# User events
+
+USER_CREATED = 'UserCreated'
+USER_DELETED = 'UserDeleted'
+USER_UPDATED = 'UserUpdated'
+
+# Article events
+
+ARTICLE_CREATED = "ArticleCreated"
+ARTICLE_UPDATED = "ArticleUpdated"
+ARTICLE_DELETED = "ArticleDeleted"
