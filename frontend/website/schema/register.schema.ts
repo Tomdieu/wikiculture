d8384bea776b31ca94a08ff2b18daa67a7d6c0a4
Tomdieu <ivantomdio@gmail.com>
2024-05-10 18:31:25 +0100

    feat : register schema

--- frontend/website/schema/register.schema.ts
+++ frontend/website/schema/register.schema.ts
@@ -6,7 +6,7 @@ export const signUpSchema = z.object({
     username: z.string().min(5, {message: "username should be at least 5 characters"}).max(100).refine(async (username) => {
         if (username=== "") return false
         if (username.length < 5) return false
-        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/accounts/check_username/?username=" + username)
+        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/check_username/" + username)
         const jsonData = await res.json()
         const {exists} = jsonData
         return exists !== true
