4dd5a4bcfe27384b7136c677ebe8efde7f3060e1
Tomdieu <ivantomdio@gmail.com>
2024-05-10 18:20:03 +0100

    feat : added article Version component

---
+++ frontend/website/components/verison.tsx
@@ -0,0 +1,38 @@
+import { ArticleHistoryType, ArticleType } from "@/types";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Cover from "./Cover";
+import Toolbar from "./Toolbar";
+import parse from 'html-react-parser';
+
+type VersionProps = {
+  version: ArticleHistoryType;
+};
+
+const Version = ({ version }: VersionProps) => {
+  return (
+    <Card className="shadow-lg rounded-md p-4">
+      <CardHeader>
+        <Cover preview={true} url={version.cover_image} />
+      </CardHeader>
+      <CardContent>
+        <Toolbar preview article={version as ArticleType} />
+        <article className="prose">
+            {parse(version.content)}
+        </article>
+      </CardContent>
+      <CardFooter>
+        
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default Version;
