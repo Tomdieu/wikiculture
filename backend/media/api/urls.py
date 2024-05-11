980203231c0eabe8c1f35b2359a88c232478d3d5
Tomdieu <ivantomdio@gmail.com>
2024-05-11 16:29:23 +0100

    feat : added routes [replace-file,delete-file]

--- backend/media/api/urls.py
+++ backend/media/api/urls.py
@@ -1,10 +1,15 @@
 from rest_framework.routers import DefaultRouter
-
+from django.urls import path
 from . import views
 
 router = DefaultRouter()
 
-router.register('upload', views.FileUploadViewSet,basename="upload")
-router.register("media",views.MediaViewSet,basename="media")
+router.register("upload", views.FileUploadViewSet, basename="upload")
+router.register("media", views.MediaViewSet, basename="media")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("replace-file/", views.FileReplaceView.as_view()),
+    path("delete-file/", views.DeleteFileView.as_view()),
+]
+
+urlpatterns += router.urls
