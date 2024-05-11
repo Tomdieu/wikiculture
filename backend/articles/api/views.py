d5df2bf0b1d790c11e3d20760e59bc9e160c6cc7
Tomdieu <ivantomdio@gmail.com>
2024-05-11 16:20:41 +0100

    remove comments

--- backend/articles/api/views.py
+++ backend/articles/api/views.py
@@ -62,22 +62,6 @@ class ArticleViewSet(GenericViewSet, CreateModelMixin, RetrieveModelMixin, ListM
         headers = self.get_success_headers(serializer.data)
         return Response(ArticleListSerializer(instance).data, status=status.HTTP_201_CREATED, headers=headers)
     
-    # def update(self, request, *args, **kwargs):
-    #     partial = kwargs.pop('partial', False)
-    #     instance = self.get_object()
-    #     print("Body : ")
-    #     print(request.data)
-    #     # serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    #     # serializer.is_valid(raise_exception=True)
-    #     # self.perform_update(serializer)
-
-    #     # if getattr(instance, '_prefetched_objects_cache', None):
-    #         # If 'prefetch_related' has been applied to a queryset, we need to
-    #         # forcibly invalidate the prefetch cache on the instance.
-    #         # instance._prefetched_objects_cache = {}
-
-    #     return Response(ArticleListSerializer(instance).data)
-    
     
     @action(methods=['get'],detail=False)
     def mine(self,request,*args, **kwargs):
