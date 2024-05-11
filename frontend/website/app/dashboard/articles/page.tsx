6488e91bea40b90260a0c0d741fabe0de7f75843
Tomdieu <ivantomdio@gmail.com>
2024-05-11 19:29:02 +0100

    feat : added all articles page

--- frontend/website/app/dashboard/articles/page.tsx
+++ frontend/website/app/dashboard/articles/page.tsx
@@ -1,10 +1,14 @@
+import ArticleTable from '@/components/table/article/ArticleTable'
 import React from 'react'
 
 type Props = {}
 
 const AllArticles = (props: Props) => {
   return (
-    <div>AllArticles</div>
+    <div className='container mx-auto h-full w-full space-y-2'>
+      <h1 className='font-bold text-xl lg:text-2xl xl:text-4xl'>Articles</h1>
+      <ArticleTable/>
+    </div>
   )
 }
 
