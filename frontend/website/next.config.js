c81f0f9a81b74969515c458a6ea36f0191893b9a
Tomdieu <ivantomdio@gmail.com>
2024-05-11 20:10:09 +0100

    feat : enable images rendering from all domains

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -4,8 +4,12 @@ const nextConfig = {
         remotePatterns:[
             {
                 protocol: 'https',
-                hostname:"**"
-            }
+                hostname:"**",
+            },
+            {
+                protocol: 'http',
+                hostname:"**",
+            },
         ]
     }
 }
