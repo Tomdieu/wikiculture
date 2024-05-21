00ba2316e29f0ebf8ab15164773715db143a9a28
tomdieu <ivantomdio@gmail.com>
2024-05-21 01:13:39 +0100

    feat : updated run.sh

--- backend/moderator/run.sh
+++ backend/moderator/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source ./env/bin/activate && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8005 & source ./env/bin/activate && python consumer.py
+source ./env/bin/activate && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8005 & source ./env/bin/activate && python manage.py makemigrations && python manage.py migrate && python consumer.py
