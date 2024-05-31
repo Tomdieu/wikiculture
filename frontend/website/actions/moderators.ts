4f7795e6327741cb034b016f3a098538aa6acb3a
Tomdieu <ivantomdio@gmail.com>
2024-05-31 23:38:56 +0100

    fix

--- frontend/website/actions/moderators.ts
+++ frontend/website/actions/moderators.ts
@@ -30,13 +30,13 @@ export const getArticleToModerate = async (page?: string) => {
   }
 };
 
-export const moderateArticle = async (data: ModerationRecordCreateType) => {
+export const moderateArticle = async (body: ModerationRecordCreateType) => {
   try {
     const session = await getSession();
     const url = `${process.env.NEXT_PUBLIC_MODERATOR_URL}/api/moderate-article/`;
     const res = await fetch(url, {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(body),
       headers: {
         Authorization: `token ${session?.user.token}`,
         "Content-Type": "application/json",
