4bcb97f4784dd008e89f931a7cd536b185cd4035
Tomdieu <ivantomdio@gmail.com>
2024-06-01 18:51:40 +0100

    feat : updated

--- backend/articles/articles/settings.py
+++ backend/articles/articles/settings.py
@@ -166,7 +166,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("api.authentication.TokenAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 # SWAGGER
