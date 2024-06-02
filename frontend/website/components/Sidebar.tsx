5b1af015fc5dc0c0bb6c510ce76ed199f270f245
Tomdieu <ivantomdio@gmail.com>
2024-06-02 20:51:02 +0100

    style : updated

--- frontend/website/components/Sidebar.tsx
+++ frontend/website/components/Sidebar.tsx
@@ -1,14 +1,14 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { ChevronRight, Dot, LoaderIcon } from "lucide-react";
+import { ChevronRight, LoaderIcon } from "lucide-react";
 
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import sidebarData, { filterSidebarDataByUserType } from "@/constants/sidebarLinks";
+import { filterSidebarDataByUserType } from "@/constants/sidebarLinks";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
@@ -18,26 +18,26 @@ type Props = {};
 
 const Sidebar = (props: Props) => {
   const path = usePathname();
-  const {data,isError,isLoading} = useQuery({
-    queryKey:["user"],
-    queryFn:()=>getSession()
-    
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getSession()
+
   })
   return (
-    <div className="w-full flex flex-col h-screen overflow-y-auto">
-      <div className="py-5 px-4 border-b border-b-stone-100 dark:border-b-stone-600 flex items-center justify-between relative">
+    <div className="w-full flex flex-col h-screen flex-1 overflow-y-auto relative">
+      <div className="py-5 px-4 border-b border-b-stone-100 dark:border-b-stone-600 flex items-center justify-between sticky top-0 bg-white dark:bg-stone-950">
         <Link href={"/dashboard"}>
           <h1 className="transition-all duration-500 text-xl sm:text-2xl lg:text-3xl font-medium cursor-pointer select-none">
             WikiCulture
           </h1>
         </Link>
       </div>
-      <div className="flex flex-col space-y-3 px-4 py-3 justify-center h-full">
+      <div className="flex flex-col space-y-3 px-4 py-3 justify-center">
         <div className="flex flex-col space-y-2 flex-1">
           {isLoading && (
             <div className="flex flex-1 w-full h-full items-center justify-center">
-              <LoaderIcon className="animate-spin h-5 w-5"/>
-              </div>
+              <LoaderIcon className="animate-spin h-5 w-5" />
+            </div>
           )}
           {!isError && data?.user && filterSidebarDataByUserType(data.user.user_type).map((item, index) => (
             <React.Fragment key={index}>
@@ -47,13 +47,13 @@ const Sidebar = (props: Props) => {
                     <div
                       className={cn(
                         "rounded-md select-none flex items-center justify-between space-x-3 py-3 px-1 cursor-pointer w-full"
-                      ,)}
+                        ,)}
                     >
                       <div className="flex items-center space-x-3 flex-1 w-full">
                         <div className="border-none rounded-md flex items-center justify-center p-2">
                           <item.icon className="md:h-5 md:w-5 lg:w-6 lg:h-6" />
                         </div>
-                        <span className="text-xs sm:text-sm md:text-base ">
+                        <span className="text-xs sm:text-sm md:text-base line-clamp-1 ">
                           {item.text}
                         </span>
                       </div>
@@ -64,62 +64,46 @@ const Sidebar = (props: Props) => {
                     <div className="flex flex-col space-y-2 border-l">
                       {item.sublinks.map((sublink, subIndex) => (
                         <Link href={sublink.link} key={subIndex} >
-                         
-                          <div
-                            className={cn(
-                              "flex items-center space-x-3 flex-1 cursor-pointer text-none truncate ml-1 py-1",
-                              sublink.link === path &&
-                                "bg-primary text-stone-50 dark:text-stone-950 rounded-md "
-                            )}
-                          >
-                            <div className="border-none rounded-md flex items-center justify-center p-2">
-                              <sublink.icon className="md:h-5 md:w-5 lg:w-6 lg:h-6" />
-                            </div>
-                            <span className="text-xs sm:text-sm md:text-base">
-                              {sublink.text}
-                            </span>
+
+                        <div
+                          className={cn(
+                            "flex items-center space-x-3 flex-1 cursor-pointer text-none truncate ml-1 py-1",
+                            sublink.link === path &&
+                            "bg-primary text-stone-50 dark:text-stone-950 rounded-md "
+                          )}
+                        >
+                          <div className="border-none rounded-md flex items-center justify-center p-2">
+                            <sublink.icon className="md:h-5 md:w-5 lg:w-6 lg:h-6" />
                           </div>
-                        </Link>
+                          <span className="text-xs sm:text-sm md:text-base line-clamp-1">
+                            {sublink.text}
+                          </span>
+                        </div>
+                      </Link>
+
                       ))}
                     </div>
                   </CollapsibleContent>
                 </Collapsible>
               ) : (
-                // <Link href={item.link} >
-                //   <div
-                //     className={cn(
-                //       "rounded-md select-none flex items-center justify-between space-x-3 py-1 px-3 cursor-pointer w-full",
-                //       item.link === path &&
-                //         "bg-primary text-stone-50 dark:text-stone-950"
-                //     )}
-                //   >
-                //     <div className="flex items-center space-x-3 flex-1 w-full">
-                //       <div className="border-none rounded-md flex items-center justify-center p-2 truncate">
-                //         <item.icon size={24} className="md:h-5 md:w-5 lg:w-6 lg:h-6"/>
-                //       </div>
-                //       <span className="text-xs sm:text-sm md:text-base">
-                //         {item.text}
-                //       </span>
-                //     </div>
-                //   </div>
-                // </Link>
+                
                 <Link href={item.link} key={index} >
-                         
-                <div
-                  className={cn(
-                    "flex items-center space-x-3 flex-1 cursor-pointer text-none truncate ml-1 py-1",
-                    item.link === path &&
+
+                  <div
+                    className={cn(
+                      "flex items-center space-x-3 flex-1 cursor-pointer text-none truncate ml-1 py-1",
+                      item.link === path &&
                       "bg-primary text-stone-50 dark:text-stone-950 rounded-md "
-                  )}
-                >
-                  <div className="border-none rounded-md flex items-center justify-center p-2">
-                    <item.icon className="md:h-5 md:w-5 lg:w-6 lg:h-6" />
+                    )}
+                  >
+                    <div className="border-none rounded-md flex items-center justify-center p-2">
+                      <item.icon className="md:h-5 md:w-5 lg:w-6 lg:h-6" />
+                    </div>
+                    <span className="text-xs sm:text-sm md:text-base">
+                      {item.text}
+                    </span>
                   </div>
-                  <span className="text-xs sm:text-sm md:text-base">
-                    {item.text}
-                  </span>
-                </div>
-              </Link>
+                </Link>
               )}
             </React.Fragment>
           ))}
