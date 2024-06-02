7c0e3b7c2d869b5e09749036672722950f5cd8a4
Tomdieu <ivantomdio@gmail.com>
2024-06-02 20:38:35 +0100

    style : updated Article styles

--- frontend/website/app/(marketing)/_components/Article.tsx
+++ frontend/website/app/(marketing)/_components/Article.tsx
@@ -23,31 +23,31 @@ const Article = ({ article }: Props) => {
             {article.categories.map((cat) => (
               <div className="rounded-full bg-gray-50 px-3 py-1.5 ">
                 <a
-                href="#"
-                className="relative z-10 text-xs font-medium text-gray-600 hover:bg-gray-100 overflow-hidden text-ellipsis whitespace-nowrap"
-                key={cat.id} // Ensure each element has a unique key if using map
-              >
-                <span className="sr-only">{cat.name}</span>
-                {cat.name.toUpperCase()}
-              </a>
+                  href="#"
+                  className="relative z-10 text-xs font-medium text-gray-600 hover:bg-gray-100 overflow-hidden text-ellipsis whitespace-nowrap"
+                  key={cat.id} // Ensure each element has a unique key if using map
+                >
+                  <span className="sr-only">{cat.name}</span>
+                  {cat.name.toUpperCase()}
+                </a>
               </div>
             ))}
           </div>
         </div>
       </div>
       <div className="group flex-1">
-        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+        <h3 className="mt-3 text-lg font-semibold leading-6 text-muted-foreground group-hover:text-muted-foreground/85">
           <a href="#">
             <span className=""></span>
             {article.title}
           </a>
         </h3>
         <p className="mt-5 line-clamp-4 text-sm leading-6 text-stone-800 dark:text-stone-50">
-          {DOMPurify.sanitize(article.content, { ALLOWED_TAGS: ["b"] })}
+          {DOMPurify.sanitize(article.content, { ALLOWED_TAGS: ["b"], })}
         </p>
       </div>
       <div className="mt-8 flex items-center gap-x-4">
-        
+
         <Avatar className="rounded-full p-0.5 w-10 h-10">
           <AvatarImage
             className="h-10 w-10 rounded-full bg-gray-50"
