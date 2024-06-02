243c53145f9e1c2e3febef01a0fd226293653a18
Tomdieu <ivantomdio@gmail.com>
2024-06-02 20:41:39 +0100

    feat : updated sytle

--- frontend/website/app/(marketing)/explore/page.tsx
+++ frontend/website/app/(marketing)/explore/page.tsx
@@ -1,19 +1,46 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/WRKWPXWQ5ap
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
+"use client"
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { CatIcon, FilterIcon } from "lucide-react"
-import { getLatestArticles } from "@/actions/articles"
+import { CatIcon, FilterIcon, Loader, TagIcon } from "lucide-react"
+import { getExploreFilteredArticle, getLatestArticles } from "@/actions/articles"
 import { cleanHtml } from "@/lib/cleanHtml"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { getFullName } from "@/lib/getFullName"
+import { useFilterStore } from "@/hooks/use-filter"
+import { useExploreFilter } from "@/hooks/user-explore-filter"
+import { useQuery } from "@tanstack/react-query"
+import { Skeleton } from "@/components/ui/skeleton"
+import ArticleCardSkeleton from "../articles/_components/_components/ArticleCardSkeleton"
+import { articleFilter } from "@/constants/articleFilter"
 
-export default async function ExplorePages() {
-  const latestArticles = await getLatestArticles()
+export default function ExplorePages() {
+  const filters = useFilterStore()
+  const exploreFilter = useExploreFilter()
+
+  const { data: categories, isLoading: isCategoriesLoading } = useQuery({
+    queryKey: ["categories"],
+    queryFn: () => filters.getCategories(),
+  });
+
+  const { data: articles, isLoading } = useQuery({
+    queryKey: ["article-filter", "filter", exploreFilter.articleFilter, "category", exploreFilter.categoryFilter],
+    queryFn: () => getExploreFilteredArticle(exploreFilter.articleFilter, exploreFilter.categoryFilter)
+  })
+
+
+  const getFilter = () => {
+    if (exploreFilter.articleFilter === "newest") {
+      return "Latest"
+    }
+    if (exploreFilter.articleFilter === "popular") {
+      return "Popular"
+    }
+    if (exploreFilter.articleFilter === "most_read") {
+      return "Most read"
+    }
+  }
+
   return (
     <div>
       <section className="relative h-[500px] md:h-[600px] lg:h-[700px] overflow-hidden">
@@ -35,11 +62,16 @@ export default async function ExplorePages() {
           </p>
         </div>
       </section>
-      <section className="py-12 md:py-16 lg:py-20">
-        <div className="container px-4 md:px-6">
+      <section className="py-12 md:py-16 lg:py-20 min-h-[500px] md:min-h-[600px] lg:min-h-[700px] flex flex-col">
+        <div className="container px-4 md:px-6 h-full w-full">
           <div className="flex items-center justify-between mb-8">
-            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">Latest Articles</h2>
-            <div className="flex items-center gap-4">
+            <div>
+              <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">{getFilter()} Articles </h2>
+              <span className="font-semibold text-sm">Category : {exploreFilter.categoryFilter ? exploreFilter.categoryFilter : "All"}</span>
+
+
+            </div>
+            <div className="flex items-center gap-4 h-full">
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button variant="outline" size="sm">
@@ -48,65 +80,81 @@ export default async function ExplorePages() {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-[200px]" align="end">
-                  <DropdownMenuRadioGroup value="featured">
-                    <DropdownMenuRadioItem value="featured">Featured</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="newest">Newest</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="popular">Popular</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="category">Category</DropdownMenuRadioItem>
+                  <DropdownMenuRadioGroup value={exploreFilter.articleFilter}>
+                    {articleFilter.map((filter) => (
+
+                      <DropdownMenuRadioItem className="text-xs cursor-pointer" value={filter.value} onSelect={() => exploreFilter.setAricleFilter(filter.value)}>{filter.label}</DropdownMenuRadioItem>
+                    ))}
                   </DropdownMenuRadioGroup>
                 </DropdownMenuContent>
               </DropdownMenu>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button variant="outline" size="sm">
-                    <CatIcon className="w-4 h-4 mr-2" />
+                    <TagIcon className="w-4 h-4 mr-2" />
                     Category
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-[200px]" align="end">
-                  <DropdownMenuRadioGroup value="all">
-                    <DropdownMenuRadioItem value="all">All</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="arts">Arts</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="culture">Culture</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="lifestyle">Lifestyle</DropdownMenuRadioItem>
+                  <DropdownMenuRadioGroup value={exploreFilter.categoryFilter}>
+                    <DropdownMenuRadioItem value="" onSelect={() => exploreFilter.setCategory("")}>All</DropdownMenuRadioItem>
+                    {isCategoriesLoading && (<Loader className="w-4 h-4 animate-spin" />)}
+                    {categories?.map((cat, index) => (
+                      <DropdownMenuRadioItem className="text-xs cursor-pointer" key={index} onSelect={(e) => {
+                        exploreFilter.setCategory(cat.name)
+                      }} value={cat.name} >{cat.name}</DropdownMenuRadioItem>
+
+                    ))}
                   </DropdownMenuRadioGroup>
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {latestArticles.map((article,index)=>(
-              <div key={index} className="bg-white dark:bg-gray-950 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <Link href={`/articles/${article.id}/`} className="block" prefetch={false}>
-                <img
-                  src={article.cover_image}
-                  alt="Article Image"
-                  width={400}
-                  height={225}
-                  className="w-full h-[225px] object-cover"
-                />
-                <div className="p-4">
-                  <h3 className="text-lg font-semibold mb-2">{article.icon}{article.title}</h3>
-                  <p className="text-gray-500 text-sm dark:text-gray-400 mb-4 line-clamp-4">
-                    {cleanHtml(article.content)}
-                  </p>
-                  <div className="flex items-center text-gray-500 dark:text-gray-400">
-                  <Avatar className="rounded-full p-0.5 w-10 h-10">
-          <AvatarImage
-            className="h-10 w-10 rounded-full bg-gray-50"
-            src={article.author.image || ""}
-          />
-          <AvatarFallback className="rounded-full h-10 w-10  shadow-sm uppercase">
-            {article.author.username.charAt(0)}
-          </AvatarFallback>
-        </Avatar>
-                    <span>{getFullName(article.author)}</span>
-                  </div>
-                </div>
-              </Link>
+          {isLoading && (
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+              {Array.from({ length: 6 }).map((_, index) => (
+                <ArticleCardSkeleton key={index} />
+              ))}
             </div>
+          )}
+          {articles?.results.length === 0 && (<div className="w-full h-full flex items-center justify-center">
+            <p className="font-bold text-2xl">No articles found</p>
+          </div>
+          )}
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {articles?.results.map((article, index) => (
+              <div key={index} className="bg-white dark:bg-gray-950 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+                <Link href={`/articles/${article.id}/`} className="block" prefetch={false}>
+                  <img
+                    src={article.cover_image}
+                    alt="Article Image"
+                    width={400}
+                    height={225}
+                    className="w-full h-[225px] object-cover"
+                  />
+                  <div className="p-4">
+                    <h3 className="text-lg font-semibold mb-2">{article.icon}{article.title}</h3>
+                    <p className="text-gray-500 text-sm dark:text-gray-400 mb-4 line-clamp-4">
+                      {cleanHtml(article.content)}
+                    </p>
+                    <div className="flex items-center text-gray-500 dark:text-gray-400">
+                      <Avatar className="rounded-full p-0.5 w-10 h-10">
+                        <AvatarImage
+                          className="h-10 w-10 rounded-full bg-gray-50"
+                          src={article.author.image || ""}
+                        />
+                        <AvatarFallback className="rounded-full h-10 w-10  shadow-sm uppercase">
+                          {article.author.username.charAt(0)}
+                        </AvatarFallback>
+                      </Avatar>
+                      <span>{getFullName(article.author)}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
             ))}
-            
+
           </div>
         </div>
       </section>
