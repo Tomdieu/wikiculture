799f9c740008301872cd70e39eca2fd2ed40fe21
Tomdieu <ivantomdio@gmail.com>
2024-06-04 07:17:13 +0100

    feat : add getCulturalAreasByName action

--- frontend/website/actions/cultural_areas.ts
+++ frontend/website/actions/cultural_areas.ts
@@ -1,6 +1,10 @@
 "use server";
 
-import { CulturalAreaListType,CulturalAreaPaginationType } from "@/types";
+import {
+  CulturalAreaListType,
+  CulturalAreaPaginationType,
+  CulturalAreaType,
+} from "@/types";
 
 export const getAllCulturalAreas = async () => {
   try {
@@ -13,7 +17,7 @@ export const getAllCulturalAreas = async () => {
     if (!res.ok) {
       throw new Error("Failed to fetch article");
     }
-    const data = await res.json() as CulturalAreaListType[];
+    const data = (await res.json()) as CulturalAreaListType[];
     return data;
   } catch (error) {
     console.error("Error fetching all cultural areas:", error);
@@ -21,10 +25,29 @@ export const getAllCulturalAreas = async () => {
   }
 };
 
-
-export const getCulturalAreas = async (page:string="1") => {
-  try{
+export const getCulturalAreas = async (page: string = "1") => {
+  try {
     const url = `${process.env.NEXT_PUBLIC_ARTICLE_URL}/api/cultural-areas/?page=${page}`;
+    const res = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache:"no-store"
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch article");
+    }
+    const data = (await res.json()) as CulturalAreaPaginationType;
+    return data;
+  } catch (error) {
+    console.error("Error fetching cultural areas:", error);
+    throw error; // Rethrow the error to handle it at the caller level if needed
+  }
+};
+
+export const getCulturalAreasByName = async (name: string) => {
+  try {
+    const url = `${process.env.NEXT_PUBLIC_ARTICLE_URL}/api/cultural-areas/get/?name=${name}`;
     const res = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
@@ -33,10 +56,10 @@ export const getCulturalAreas = async (page:string="1") => {
     if (!res.ok) {
       throw new Error("Failed to fetch article");
     }
-    const data = await res.json() as CulturalAreaPaginationType;
+    const data = (await res.json()) as CulturalAreaType;
     return data;
-  }catch(error){
+  } catch (error) {
     console.error("Error fetching cultural areas:", error);
     throw error; // Rethrow the error to handle it at the caller level if needed
   }
-}
+};
