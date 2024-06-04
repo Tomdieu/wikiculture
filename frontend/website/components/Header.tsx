55f7912bd7e8f2ef9069bcba8a82401c96abbc35
Tomdieu <ivantomdio@gmail.com>
2024-06-04 07:24:36 +0100

    feat : updated header

--- frontend/website/components/Header.tsx
+++ frontend/website/components/Header.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { ModeToggle } from "./theme-toggle";
-import { Button } from "./ui/button";
+
 import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "./ui/dropdown-menu";
-import { MoreHorizontalIcon } from "lucide-react";
+
 import { getSession } from "@/lib/getSession";
 import UserInfo from "./user-info";
 import { Input } from "./ui/input";
+import NavLink from "./NavLink";
+import HeaderSheet from "./HeaderSheet";
+
 
 type Props = {};
 
@@ -23,82 +20,10 @@ const Header = async (props: Props) => {
         <h1 className="text-2xl font-bold">Wikiculture</h1>
       </Link>
       <div className="flex items-center space-x-3">
-        <nav className="hidden ml-auto sm:flex gap-4 sm:gap-6 items-center">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/explore"
-          >
-            Explore
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/articles"
-          >
-            Articles
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/about"
-          >
-            About
-          </Link>
-          <form method="get" action="/search">
-            <Input type="search" name="query" placeholder="Search article" />
-          </form>
-
-        </nav>
+        <NavLink />
         <ModeToggle />
         {session?.user && <UserInfo />}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant={"ghost"} size={"icon"} className="sm:hidden">
-              <MoreHorizontalIcon className="text-muted-foreground" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem asChild>
-              <Link
-                className="text-sm font-medium hover:underline underline-offset-4"
-                href="/explore"
-              >
-                Explore
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link
-                className="text-sm font-medium hover:underline underline-offset-4"
-                href="/articles"
-              >
-                Articles
-              </Link>
-            </DropdownMenuItem>
-
-            <DropdownMenuItem asChild>
-              <Link
-                className="text-sm font-medium hover:underline underline-offset-4"
-                href="/about"
-              >
-                About
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link
-                className="text-sm font-medium hover:underline underline-offset-4"
-                href="/login"
-              >
-                Sign In
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link
-                className="text-sm font-medium hover:underline underline-offset-4"
-                href="/register"
-              >
-                Sign Up
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <HeaderSheet session={session} />
       </div>
     </div>
   );
