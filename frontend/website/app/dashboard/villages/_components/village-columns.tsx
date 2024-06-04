9387d671f4eefa42c89b8edd89757fed97533e73
Tomdieu <ivantomdio@gmail.com>
2024-06-04 07:25:19 +0100

    feat : updated village columns

--- frontend/website/app/dashboard/villages/_components/village-columns.tsx
+++ frontend/website/app/dashboard/villages/_components/village-columns.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import JoditEditor from "@/components/editor/JoditEditor";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
 import { VillageType } from "@/types";
 import { ColumnDef } from "@tanstack/react-table";
+import DOMPurify from "dompurify";
+import { MoreHorizontalIcon } from "lucide-react";
 import Link from "next/link";
 
 
@@ -38,10 +44,10 @@ export const columns: ColumnDef<VillageType>[] = [
         accessorKey: "description",
         header: "Description",
         cell({ row }) {
-          const culturalArea = row.original;
+          const village = row.original;
           return (
             <p className="font-medium text-ellipsis overflow-hidden line-clamp-1">
-              {culturalArea.description}
+              {DOMPurify.sanitize(village.description)}
             </p>
           );
         },
@@ -69,5 +75,43 @@ export const columns: ColumnDef<VillageType>[] = [
               </p>
             ); 
         }
-      }
+      },
+      {
+        id: "options",
+        cell: ({ row }) => {
+          const village = row.original;
+          return (
+            <Dialog onOpenChange={() => {}}>
+              <DialogTrigger asChild>
+                <Button variant={"ghost"} size={"icon"}>
+                  <MoreHorizontalIcon className="text-muted-foreground w-4 h-4" />
+                  <span className="sr-only">More</span>
+                </Button>
+              </DialogTrigger>
+              <DialogContent
+                className={cn("w-full lg:max-w-5xl p-0 overflow-y-auto prose max-w-fit",{"h-full":village.description.length > 200})}
+                forceMount
+              >
+                {village.description.length === 0 ? (
+                  <div className="w-full h-full items-center justify-center flex">
+                    <h1>No Descriptions for this cultural area</h1>
+                  </div>
+                ):(
+                  <JoditEditor
+                  toolbar={false}
+                  disable
+                  readonly
+                  safeMode
+                  statusbar={false}
+                  value={village.description}
+                  onChange={() => {}}
+                  className={"prose prose-blue"}
+                />
+                )}
+                
+              </DialogContent>
+            </Dialog>
+          );
+        },
+      },
 ]
