5a169f74a7cb469d4c510f2a6e03cea961adbc7c
ivantomdio <ivantomdio@gmail.com>
2024-06-05 14:45:51 +0000

    updated dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,10 +7,14 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
+    pkgs.sudo
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    # pkgs.docker
+    pkgs.nvidia-docker
+    pkgs.docker-compose
   ];
 
   # Sets environment variables in the workspace
