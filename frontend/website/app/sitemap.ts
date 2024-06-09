2b4800d2c684bdb25add86210da88857ba6cd97a
tomdieu <ivantomdio@gmail.com>
2024-06-09 22:59:02 +0100

    fix : sitemap problems

--- frontend/website/app/sitemap.ts
+++ frontend/website/app/sitemap.ts
@@ -1,65 +1,106 @@
+import { MetadataRoute } from "next";
 import { getAllArticles, getArticles } from "@/actions/articles";
 import { getCulturalAreas } from "@/actions/cultural_areas";
+import { ArticleType, CulturalAreaPaginationType } from "@/types";
 
-export default async function sitemap() {
+type CustomSitemap = {
+  url: string;
+  lastModified?: string | Date;
+  changeFrequency?: string;
+  priority?: number;
+};
+
+export default async function sitemap(): Promise<CustomSitemap[]> {
   const baseUrl = "https://wikiculture.vercel.app";
+  let articles: ArticleType[] = [];
+  let culturalAreas: CulturalAreaPaginationType | null = null;
 
-  const articles = await getAllArticles();
-  const culturalAreas = await getCulturalAreas()
+  try {
+    articles = await getAllArticles();
+  } catch (error) {}
 
+  try {
+    culturalAreas = await getCulturalAreas();
+  } catch (error) {}
 
   const articleUrls = articles.map((article) => ({
     url: `${baseUrl}/articles/${article.id}`,
-    lastModified: new Date(),
+    lastModified: article.updated_at,
+    changeFrequency: "weekly",
+    priority: 1,
   }));
 
-  const culturalAreaUrls = culturalAreas.results.map((cult)=>({
-    url: `${baseUrl}/articles/${cult.name}`,
-    lastModified: new Date(),
-  }))
-  
+  const culturalAreaUrls = culturalAreas?.results
+    ? culturalAreas.results.map((cult) => ({
+        url: `${baseUrl}/cultural-regions/${cult.name}`,
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 1,
+      }))
+    : [];
+
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/login`,
       lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 1,
     },
     {
       url: `${baseUrl}/register`,
       lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 1,
     },
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 1,
     },
     {
       url: `${baseUrl}/search`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/articles`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/explore`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/cultural-regions`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/explore`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/explore`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     ...articleUrls,
-    ...culturalAreaUrls
+    ...culturalAreaUrls,
   ];
 }
