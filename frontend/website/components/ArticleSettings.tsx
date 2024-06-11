0c06a91d7ec4af9497ccf596a1e64aed5005ab8d
tomdieu <ivantomdio@gmail.com>
2024-06-11 22:26:32 +0100

    feat : added article deletion

--- frontend/website/components/ArticleSettings.tsx
+++ frontend/website/components/ArticleSettings.tsx
@@ -1,65 +1,140 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-import { Button } from './ui/button'
-import { FileStack, MoreHorizontal, SaveAllIcon, Trash } from 'lucide-react'
-import { ArticleType } from '@/types'
-import { useArticleStore } from '@/hooks/use-article-store'
+import { Button } from "./ui/button";
+import {
+  FileStack,
+  Loader2,
+  MoreHorizontal,
+  SaveAllIcon,
+  Trash,
+} from "lucide-react";
+import { ArticleType } from "@/types";
+import { useArticleStore } from "@/hooks/use-article-store";
+import { deleteArticle } from "@/actions/articles";
+import { revalidatePage } from "@/actions/revalidate";
+import { useRouter } from "next/navigation";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { cn } from "@/lib/utils";
+import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 type Props = {
-    article:ArticleType
-}
+  article: ArticleType;
+};
 
-const ArticleSettings = ({article}: Props) => {
+const ArticleSettings = ({ article }: Props) => {
+  const { saveArticle, openVersionModel } = useArticleStore();
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const [deleting, setDeleting] = useState(false);
 
-    const {saveArticle,openVersionModel} = useArticleStore()
+  const queryClient = useQueryClient();
 
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant={"ghost"} size={"icon"}>
-                    <MoreHorizontal className='text-muted-foreground'/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className='w-56' align='end' alignOffset={8} forceMount>
-                <DropdownMenuLabel>Settings</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <div className='w-full space-y-1 space-x-1'>
-                    <DropdownMenuGroup>
+  const handleDelete = () => {
+    setDeleting(true);
+    deleteArticle(article.id)
+      .then(() => {
+        setDeleting(false);
+        queryClient.invalidateQueries({ queryKey: ["articles", "page", "1"] });
+        router.replace("/dashboard/articles/");
+        router.refresh()
+        revalidatePage("/dashboard/articles/");
 
-                    <DropdownMenuItem onClick={saveArticle} className='cursor-pointer text-sm w-full flex items-center text-green-500 dark:text-green-500'>
-                            
-                            <SaveAllIcon className='w-4 h-4 mr-2' />
-                            Save
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={openVersionModel} className='cursor-pointer text-sm w-full flex items-center text-blue-500 dark:text-blue-500'>
-                            
-                            <FileStack className='w-4 h-4 mr-2' />
-                            Versions
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
+      })
+      .finally(() => {
+        toast.success("Article deleted successfully")
 
-                    <DropdownMenuSeparator className='my-4' />
-                    <DropdownMenuGroup>
+        queryClient.invalidateQueries({ queryKey: ["articles", "page", "1"] });
+        setDeleting(false);
+      });
+  };
 
-                        <DropdownMenuItem className='cursor-pointer text-sm w-full flex items-center text-red-400 dark:text-red-300'>
-                            <Trash className='w-4 h-4 mr-2' />
-                            Delete Article
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                </div>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
-}
+  return (
+    <>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confim Delete?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Do you really want to delete the article.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={deleting}>Cancel</AlertDialogCancel>
+            <AlertDialogAction disabled={deleting} onClick={handleDelete}>
+              <Loader2 className={cn("w-4 h-4 animate-spin",{
+                "hidden":!deleting
+              })} /> Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} size={"icon"}>
+            <MoreHorizontal className="text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="w-56"
+          align="end"
+          alignOffset={8}
+          forceMount
+        >
+          <DropdownMenuLabel>Settings</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <div className="w-full space-y-1 space-x-1">
+            <DropdownMenuGroup>
+              <DropdownMenuItem
+                onClick={saveArticle}
+                className="cursor-pointer text-sm w-full flex items-center text-green-500 dark:text-green-500"
+              >
+                <SaveAllIcon className="w-4 h-4 mr-2" />
+                Save
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={openVersionModel}
+                className="cursor-pointer text-sm w-full flex items-center text-blue-500 dark:text-blue-500"
+              >
+                <FileStack className="w-4 h-4 mr-2" />
+                Versions
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
 
-export default ArticleSettings
+            <DropdownMenuSeparator className="my-4" />
+            <DropdownMenuGroup>
+              <DropdownMenuItem
+                onClick={() => setOpen(true)}
+                className="cursor-pointer text-sm w-full flex items-center text-red-400 dark:text-red-300"
+              >
+                <Trash className="w-4 h-4 mr-2" />
+                Delete Article
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default ArticleSettings;
--- frontend/website/components/table/article/action.tsx
+++ frontend/website/components/table/article/action.tsx
@@ -10,9 +10,25 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useOrigin } from "@/hooks/use-origin";
 import { ArticleType } from "@/types";
-import { Copy, Delete, MoreHorizontalIcon, Trash } from "lucide-react";
-import React from "react";
+import { Copy, Delete, Loader2, MoreHorizontalIcon, Trash } from "lucide-react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
+import { deleteArticle } from "@/actions/articles";
+import { revalidatePage } from "@/actions/revalidate";
+import { cn } from "@/lib/utils";
+import { useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   article: ArticleType;
@@ -25,35 +41,89 @@ const ActiclesAction = ({ article }: Props) => {
     navigator.clipboard.writeText(url);
     toast.success("Url copied");
   };
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const [deleting, setDeleting] = useState(false);
+
+  const queryClient = useQueryClient();
+
+  const handleDelete = () => {
+    setDeleting(true);
+    deleteArticle(article.id)
+      .then(() => {
+        queryClient.invalidateQueries({ queryKey: ["articles", "page", "1"] });
+        setDeleting(false);
+        router.replace("/dashboard/articles/");
+        router.refresh();
+        revalidatePage("/dashboard/articles/");
+      })
+      .finally(() => {
+        toast.success("Article deleted successfully");
+        queryClient.invalidateQueries({ queryKey: ["articles", "page", "1"] });
+        setDeleting(false);
+      });
+  };
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button size={"icon"} variant={"ghost"} className="rounded-full">
-          <MoreHorizontalIcon className="text-muted-foreground w-4 h-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel className="text-sm">Actions</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
+    <>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confim Delete?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Do you really want to delete the article.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={deleting}>Cancel</AlertDialogCancel>
+            <AlertDialogAction disabled={deleting} onClick={handleDelete}>
+              <Loader2
+                className={cn("w-4 h-4 animate-spin", {
+                  hidden: !deleting,
+                })}
+              />{" "}
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button size={"icon"} variant={"ghost"} className="rounded-full">
+            <MoreHorizontalIcon className="text-muted-foreground w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel className="text-sm">Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                onCopy();
+              }}
+              className="text-muted-foreground cursor-pointer text-xs"
+            >
+              <Copy className="w-4 h-5 mr-2" />
+              Copy Link
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
           <DropdownMenuItem
-            onClick={(e)=>{
-              e.preventDefault()
-              onCopy()
+            onClick={(e) => {
+              e.preventDefault();
+
+              e.stopPropagation();
+              setOpen(true);
             }}
-            className="text-muted-foreground cursor-pointer text-xs"
+            className="text-red-500  cursor-pointer text-xs hover:text-red-300 group"
           >
-            <Copy className="w-4 h-5 mr-2" />
-            Copy Link
+            <Trash className="w-4 h-4 mr-2 hover:text-red-500" />
+            Delete
           </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="text-red-500  cursor-pointer text-xs hover:text-red-300 group">
-          <Trash className="w-4 h-4 mr-2 hover:text-red-500" />
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
 
