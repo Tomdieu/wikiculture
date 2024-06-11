a97ce9cbf92e8083ff3430161962c64079093115
tomdieu <ivantomdio@gmail.com>
2024-06-11 22:29:15 +0100

    :package: feat : updated install.sh

--- backend/articles/install.sh
+++ backend/articles/install.sh
@@ -8,3 +8,13 @@ source ./env/bin/activate
 
 # Install requirements
 pip install -r requirements.txt
+
+# Download NLTK data
+python -c "import nltk; \
+           nltk.download('punkt'); \
+           nltk.download('stopwords'); \
+           nltk.download('averaged_perceptron_tagger'); \
+           nltk.download('wordnet'); \
+           nltk.download('maxent_ne_chunker'); \
+           nltk.download('vader_lexicon');  \
+           nltk.download('words');"
