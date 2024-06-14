cc2ae4b3f40c87c62f6823c54b06debb00f32e56
tomdieu <ivantomdio@gmail.com>
2024-06-14 11:51:47 +0100

    :drizzy: updated install command

--- backend/articles/install.sh
+++ backend/articles/install.sh
@@ -12,12 +12,18 @@ pip install -r requirements.txt
 python manage.py create_categories
 python manage.py import_data
 
-# Download NLTK data
+# Create the NLTK data directory in the current directory if it doesn't exist
+if [ ! -d "nltk_data" ]; then
+    mkdir nltk_data
+fi
+
+# Download NLTK resources to the current directory
 python -c "import nltk; \
-           nltk.download('punkt'); \
-           nltk.download('stopwords'); \
-           nltk.download('averaged_perceptron_tagger'); \
-           nltk.download('wordnet'); \
-           nltk.download('maxent_ne_chunker'); \
-           nltk.download('vader_lexicon');  \
-           nltk.download('words');"
+           nltk.data.path.append('nltk_data'); \
+           nltk.download('punkt', download_dir='nltk_data'); \
+           nltk.download('stopwords', download_dir='nltk_data'); \
+           nltk.download('averaged_perceptron_tagger', download_dir='nltk_data'); \
+           nltk.download('wordnet', download_dir='nltk_data'); \
+           nltk.download('maxent_ne_chunker', download_dir='nltk_data'); \
+           nltk.download('vader_lexicon', download_dir='nltk_data'); \
+           nltk.download('words', download_dir='nltk_data');"
